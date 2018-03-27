a51b95367c51392164e7210b59080a41193efc38
Jacqueline Franßen <jacquelinefranssen@Jacquelines-MBP.home>
2018-03-27 20:25:29 +0200

    removed kinvey, added mongodb

--- RFID_App/platforms/ios/RFIDApp/app/home/home-page.js
+++ RFID_App/platforms/ios/RFIDApp/app/home/home-page.js
@@ -1,50 +1,17 @@
-var Kinvey = require('kinvey-nativescript-sdk').Kinvey;
-var datastore = Kinvey.DataStore.collection('medication', Kinvey.DataStoreType.Cache);
-Kinvey.init({
-	apiHostname: 'https://baas.kinvey.com',
-	micHostname: 'https://auth.kinvey.com',
-	appKey: 'kid_rJLgyYUqf',
-	appSecret: 'e3b139db758a43a7af2515ec5fba6274'
-  });
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient
+ , assert = require('assert');
 
-  Kinvey.ping()
-  .then(function(response){
-	  alert("successfully connected to Kinvey database");
-	  console.log('Kinvey Ping Success.Kinvey Ping Success.');
-  })
-  .catch(function(error){
-	  console.log('Kinvey Ping Failed.' + error.description);
-  });
+var MongoClient = require('mongodb').MongoClient
+ , assert = require('assert');
 
-  var promise = datastore.pull().then(function(entities){
-
-  }).catch(function(error){
-
-  });
-
-  var subscription = datastore.findById("2fdfb5cd97e4418576c1dc88d309872")
-  .subscribe(function(entity){
-	alert(entity);
-  }, function(error){
-	alert("error");
-  }, function(){
-	alert("nothing");
-  });
-
-  var entity = {
-		_id: "2fdfb5cd97e4418576c1dc88d309872",
-	    _rev: "3-de99cf65a84c8f0edae4b05232aee14f",
-	    name: "Retalin",
-	    countryCode: 98765436,
-	    size: "30 pcs",
-	    location: "C10B50",
-	    timeStamp: 128293477
-  }
-
-  var promise  = datastore.save(entity)
-  .then(function(entity){
-	alert(entity);
-  }).catch(function(error){
-	alert("entity not stored");
-  })
-
+ console.log("Hello");
+// Connection URL
+var url = 'mongodb://127.0.0.1:27017/medication';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+alert("hello");
+});
